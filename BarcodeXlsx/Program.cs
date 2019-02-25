297b55025431485ce2a326277d4f6a19dd309927
Kunihiro Narita <kunihiro_narita@yahoo.co.jp>
2019-02-25 16:28:04 +0900

    add scanning barcode tag.

--- BarcodeXlsx/Program.cs
+++ BarcodeXlsx/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using ClosedXML;
+using ClosedXML.Excel;
 
 namespace BarcodeXlsx
 {
@@ -8,7 +13,28 @@ namespace BarcodeXlsx
         {
             DecodeArgumentParamaters param = new DecodeArgumentParamaters(args);
 
-            Console.WriteLine("Hello World!");
+            XLWorkbook book = new XLWorkbook(param.sourceFileName);
+            foreach(var sheet in book.Worksheets)
+            {
+                foreach (var cell in sheet.Cells())
+                {
+                    string cellValue = cell.GetString();
+                    if (cellValue.Length > param.preChars.Length + param.postChars.Length)
+                    {
+                        string preChars = cellValue.Substring(0, param.preChars.Length);
+                        string postChears = cellValue.Substring(cellValue.Length - param.postChars.Length);
+
+                        if (preChars == param.preChars && postChears == param.postChars)
+                        {
+                            string barcodeData = cellValue.Substring(param.preChars.Length, cellValue.Length - param.preChars.Length - param.postChars.Length);
+                            int delimiterPos = barcodeData.IndexOf(":");
+                            string barcodeType = barcodeData.Substring(0, delimiterPos);
+                            string barcodeValue = barcodeData.Substring(delimiterPos + 1);
+                            Console.WriteLine("barcodeType = {0}, barcodeValue = {1}", barcodeType, barcodeValue);
+                        }
+                    }
+                }
+            }
         }
     }
 }
