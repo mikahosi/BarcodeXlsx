4e49ba47e1f02ee2020aab76d5d64f968092dd9d
Kunihiro Narita <narita_kunihiro@yahoo.co.jp>
2019-07-16 00:18:31 +0900

    Add Blank Web Page

---
+++ barcode/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using barcode.Models;
+
+
+namespace barcode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (this.Request.Method == "POST")
+            {
+                foreach (var form in this.Request.Form.Files)
+                {
+                    Debug.WriteLine("Index, {0}, {1}", form.Key, form.Value);
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult Convert()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ barcode/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace barcode.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
